9f109126f19e4f880e0c64aa2bad64f0221b16ed
CarlosCabo <carloscabo@gmail.com>
2019-12-12 13:06:52 +0100

    Static?

--- js/app.js
+++ js/app.js
@@ -2,11 +2,20 @@ import Vue from 'vue';
 import VuejsDialog from 'vuejs-dialog';
 import store from './store';
 import app from './app.vue';
+import { Howl, Howler } from 'howler';
 
 import '../css/app.scss';
 import 'vuejs-dialog/dist/vuejs-dialog.min.css';
 
 Vue.use(VuejsDialog);
+
+window.blaster = new Howl({
+  src: ['./sounds/blaster.mp3']
+});
+window.blaster.once('load', function () {
+  console.log('Sound play');
+  blaster.play();
+});
 
 let vm = new Vue({
   el: "#main-app",
